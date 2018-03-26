7ea41bf4a4bcce3ff19263255d9e557902884832
DangKhoa <>
2018-03-26 15:20:24 +0700

    upload source

---
+++ LapTrinhWebASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LapTrinhWebASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
